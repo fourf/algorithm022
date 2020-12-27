a6df2a913e2653c8417c042d18dab3795e5ad262
fourf <dfzhou4405@qq.com>
2020-12-27 19:05:05 +0800

    前k个高频元素

--- Week_02/leetcode347.py
+++ Week_02/leetcode347.py
@@ -6,3 +6,12 @@ class Solution:
         # return [i[0] for i in res][:k]
 
         return [i[0] for i in collections.Counter(nums).most_common(k)]
+
+        # frq = defaultdict(list)
+        # for key, cnt in Counter(nums).items():
+        #     frq[cnt].append(key)
+        # res = []
+        # for times in range(len(nums), 0, -1):
+        #     if times in frq:
+        #         res.extend(frq[times])
+        #         if len(res) >= k: return res[:k]
